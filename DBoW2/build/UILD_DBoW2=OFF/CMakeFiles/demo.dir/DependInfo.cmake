680aa9f1cf72656ff3e49462ecbe310982d99759
luoganttccqy <ledi@xinyoudi.com>
2021-11-23 21:28:21 +0800

    DBow2

---
+++ DBoW2/build/UILD_DBoW2=OFF/CMakeFiles/demo.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ledi/slam_md40/learn_orbslam/DBoW2/demo/demo.cpp" "/home/ledi/slam_md40/learn_orbslam/DBoW2/build/UILD_DBoW2=OFF/CMakeFiles/demo.dir/demo/demo.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include/DBoW2"
+  "../../include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ledi/slam_md40/learn_orbslam/DBoW2/build/UILD_DBoW2=OFF/CMakeFiles/DBoW2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

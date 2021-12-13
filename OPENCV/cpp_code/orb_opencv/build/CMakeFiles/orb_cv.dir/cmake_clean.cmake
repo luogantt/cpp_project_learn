98f57dbc4c4d0ba3b11fdaa25233d65c2c563c3d
luoganttccqy <ledi@xinyoudi.com>
2021-12-13 13:21:36 +0800

    orb_oprencv

---
+++ OPENCV/cpp_code/orb_opencv/build/CMakeFiles/orb_cv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orb_cv.dir/orb_cv.cpp.o"
+  "orb_cv"
+  "orb_cv.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orb_cv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OPENCV/cpp_code/orb_opencv/cmake-build-debug/CMakeFiles/orb_cv.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ledi/桌面/clion/makfile_learn/slambook2/ch7/orb_opencv/orb_cv1.cpp" "/home/ledi/桌面/clion/makfile_learn/slambook2/ch7/orb_opencv/cmake-build-debug/CMakeFiles/orb_cv.dir/orb_cv1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

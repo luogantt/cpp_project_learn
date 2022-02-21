9ca1385c2bccd67197802da1afb065882864702e
luogantt <290328566@qq.com>
2022-02-21 11:29:52 +0800

    bin_to_array

---
+++ read_bin_show/main.cpp
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <fstream>
+#include <iostream>
+int main(){
+
+    /***
+    const int length = 100;
+    double f1[length] ;
+    for (int i = 0; i < length; i++)
+    {
+        f1[i] = i + i / 1000.0;
+    }
+    std::ofstream  ofs("1.txt", std::ios::binary | std::ios::out);
+    ofs.write((const char*)f1, sizeof(double) * length);
+    ofs.close();
+    ***/
+    int length = 100;
+    double* f2 = new double[length];
+    std::ifstream ifs("a.bin", std::ios::binary | std::ios::in);
+    ifs.read((char*)f2, sizeof(double) * length);
+    ifs.close();
+
+    for (int i = 0; i < length; i++)
+    {
+        std::cout<<f2[i]<<std::endl;
+    }
+    return 0;
+}
